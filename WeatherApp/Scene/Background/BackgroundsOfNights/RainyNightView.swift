ab9e85e6c6329cc85850f1d1e74f5e9cd49ed2ba
mariamsreseli <Sreseli.Mariam@tbcacademy.edu.ge>
2024-06-13 02:38:17 +0400

    adding assets and first page

---
+++ WeatherApp/Scene/Background/BackgroundsOfNights/RainyNightView.swift
@@ -0,0 +1,18 @@
+//
+//  RainyNightView.swift
+//  WeatherApp
+//
+//  Created by Mariam Sreseli on 6/13/24.
+//
+
+import SwiftUI
+
+struct RainyNightView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RainyNightView()
+}
---
+++ WeatherApp/Scene/Background/BackgroundsOfNights/WarmNightView.swift
@@ -0,0 +1,18 @@
+//
+//  WarmNightView.swift
+//  WeatherApp
+//
+//  Created by Mariam Sreseli on 6/13/24.
+//
+
+import SwiftUI
+
+struct WarmNightView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WarmNightView()
+}
