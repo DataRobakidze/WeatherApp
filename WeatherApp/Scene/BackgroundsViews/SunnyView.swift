527f3df16e30abf4413cad4c387d28eef3616b01
Data <133904834+DataRobakidze@users.noreply.github.com>
2024-06-13 15:40:42 +0400

    Update backgrounds

---
+++ WeatherApp/Scene/BackgroundsViews/SunnyView.swift
@@ -0,0 +1,49 @@
+//
+//  SunnyView.swift
+//  WeatherApp
+//
+//  Created by Data on 13.06.24.
+//
+
+import SwiftUI
+
+struct SunnyView: View {
+    var body: some View {
+        ZStack {
+            
+            VStack {
+                HStack {
+                    Spacer()
+                        .frame(width: 8)
+                    Image("Sun")
+                        .resizable()
+                        .frame(width: 138.18, height: 138.18)
+                        .foregroundStyle(
+                            LinearGradient(gradient: Gradient(colors: [Color(hex: "FFA900"), Color(hex: "FFD88B")]), startPoint: .leading, endPoint: .trailing)
+                        )
+                    VStack {
+                        Image("Tolia")
+                            .resizable()
+                            .frame(width: 30.11, height: 31.98)
+                            .padding(.leading, 10)
+                        Image("Tolia")
+                            .resizable()
+                            .frame(width: 30.11, height: 31.98)
+                        Image("Tolia")
+                            .resizable()
+                            .frame(width: 30.11, height: 31.98)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+        .background(
+            LinearGradient(gradient: Gradient(colors: [Color(hex: "2DB0DD"), Color(hex: "8EADE1")]), startPoint: .top, endPoint: .bottom)
+        )
+    }
+}
+
+#Preview {
+    SunnyView()
+}
