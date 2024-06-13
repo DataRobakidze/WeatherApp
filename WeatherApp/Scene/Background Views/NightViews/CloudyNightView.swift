4d37b61246ad6c48bea8fc835a9de0b9d60db76d
mariamsreseli <Sreseli.Mariam@tbcacademy.edu.ge>
2024-06-14 03:06:15 +0400

    adding night backgrounds

---
+++ WeatherApp/Scene/Background Views/NightViews/CloudyNightView.swift
@@ -0,0 +1,33 @@
+//
+//  CloudyNightView.swift
+//  WeatherApp
+//
+//  Created by Mariam Sreseli on 6/14/24.
+//
+
+import SwiftUI
+import SpriteKit
+
+struct CloudyNightView: View {
+    var body: some View {
+        ZStack {
+            
+            LinearGradient(
+                gradient: Gradient(colors: [Color(hex: "0D091A"), Color(hex: "37266E")]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .edgesIgnoringSafeArea(.all)
+            
+            AllScreenCloudAnimation()
+                .foregroundStyle(Color(hex: "E5E5E5"))
+            
+            MoonView()
+        }
+    }
+}
+
+#Preview {
+    CloudyNightView()
+}
+
