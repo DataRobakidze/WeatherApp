b238cdfd78bf36c3bfbe3a72771d3226fbf43d1e
Data <133904834+DataRobakidze@users.noreply.github.com>
2024-06-13 14:56:05 +0400

    Testing if upload

---
+++ WeatherApp/TestingFolder/Testing.swift
@@ -0,0 +1,20 @@
+//
+//  Testing.swift
+//  WeatherApp
+//
+//  Created by Data on 13.06.24.
+//
+
+import SwiftUI
+
+struct Testing: View {
+    var body: some View {
+        Text("1")
+        Text("2")
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    Testing()
+}
