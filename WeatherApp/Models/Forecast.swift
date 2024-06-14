f57b8a1b9f9f2250bc86b8b1eb7b8a4079e983e7
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-06-14 04:41:39 +0400

    everything up

--- WeatherApp/Models/Forecast.swift
+++ WeatherApp/Models/Forecast.swift
@@ -7,120 +7,55 @@
 
 import Foundation
 
-// MARK: - Welcome
-final class Forecast {
+
+class Forecast {
     struct Model: Decodable {
         let cod: String
-        let message, cnt: Int
         let list: [List]
         let city: City
     }
     
-    // MARK: - City
     struct City: Decodable {
         let id: Int
-        let name: String
         let coord: Coord
         let country: String
-        let population, timezone, sunrise, sunset: Int
     }
     
-    // MARK: - Coord
     struct Coord: Decodable {
         let lat, lon: Double
     }
-    
-    // MARK: - List
+  
     struct List: Decodable {
         let dt: Int
         let main: MainClass
         let weather: [Weather]
-        let clouds: Clouds
-        let wind: Wind
-        let visibility: Int
-        let pop: Double
-        let sys: Sys
         let dtTxt: String
-        let rain: Rain?
         
         enum CodingKeys: String, CodingKey {
-            case dt, main, weather, clouds, wind, visibility, pop, sys
+            case dt, main, weather
             case dtTxt = "dt_txt"
-            case rain
         }
     }
     
-    // MARK: - Clouds
-    struct Clouds: Decodable {
-        let all: Int
-    }
-    
-    // MARK: - MainClass
     struct MainClass: Decodable {
-        let temp, feelsLike, tempMin, tempMax: Double
-        let pressure, seaLevel, grndLevel, humidity: Int
-        let tempKf: Double
+        let temp, tempMin, tempMax: Double
         
         enum CodingKeys: String, CodingKey {
             case temp
-            case feelsLike = "feels_like"
             case tempMin = "temp_min"
             case tempMax = "temp_max"
-            case pressure
-            case seaLevel = "sea_level"
-            case grndLevel = "grnd_level"
-            case humidity
-            case tempKf = "temp_kf"
         }
     }
     
-    // MARK: - Rain
-    struct Rain: Decodable {
-        let the3H: Double
-        
-        enum CodingKeys: String, CodingKey {
-            case the3H = "3h"
-        }
-    }
-    
-    // MARK: - Sys
-    struct Sys: Decodable {
-        let pod: Pod
-    }
-    
-    enum Pod: String, Codable {
-        case d = "d"
-        case n = "n"
-    }
-    
-    // MARK: - Weather
     struct Weather: Decodable {
         let id: Int
         let main: MainEnum
-        let description: String  // Changed from Description enum to String
         let icon: String
     }
-    
-    // You can keep this enum if you still want to have predefined cases, but it's no longer used directly
-    enum Description: String, Codable {
-        case brokenClouds = "broken clouds"
-        case clearSky = "clear sky"
-        case fewClouds = "few clouds"
-        case lightRain = "light rain"
-        case overcastClouds = "overcast clouds"
-        case scatteredClouds = "scattered clouds"
-    }
-    
+
     enum MainEnum: String, Decodable {
         case clear = "Clear"
         case clouds = "Clouds"
         case rain = "Rain"
-    }
-    
-    // MARK: - Wind
-    struct Wind: Decodable {
-        let speed: Double
-        let deg: Int
-        let gust: Double?
     }
 }
